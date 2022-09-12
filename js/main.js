c55dda17825994efac9c12ae76f8dd796cd23cdb
michelleguti17 <gut20014@byui.edu>
2022-09-12 10:42:47 -0500

    new first commit

---
+++ js/main.js
@@ -0,0 +1,17 @@
+const links = [
+    {
+      label: "Week1 notes",
+      url: "week1/index.html",
+
+      label: "Week2 notes",
+      url: "week2/index.html",
+
+      label: "Week3 notes",
+      url: "week3/index.html",
+
+    }
+     
+     
+      
+
+  ]
